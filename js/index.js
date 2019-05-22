56df42943d50d3fd697a49a69fbd82378ab9b93e
Kevin-S <kevsilvo@gmail.com>
2019-05-22 08:22:43 -0400

    refining landing

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// When you hover over the items in the list menu it Will 
+// display an icon of a pokeball next to it by toggling a class
+//.icon is the <li> element than contains the image as a children with the icon
+//.pkicon is the class that the image inside the .icon <li> has
+
+class ICONS{
+    constructor(icon){
+        this.icon = icon;
+        this.childs = icon.querySelector('.pkicon');
+        this.icon.addEventListener('mouseover', () => this.hoverit());
+        this.icon.addEventListener('mouseout', () => this.leaveit());
+    }
+
+    hoverit(){
+        this.childs.classList.toggle('not-selected');
+       // this.childs.classList.add('selected');
+        
+    }
+
+    leaveit(){
+        this.childs.classList.toggle('not-selected');
+        //this.childs.classList.remove('selected');
+
+    }
+}
+
+//create a new instance of the ICONS class for each item available
+const selected = document.querySelectorAll('.icon');
+selected.forEach(function(element){
+    return new ICONS(element);
+});
